965707181216538999846cc05658ec89734cc7d2
richardgreckijr <richardgreckijr@gmail.com>
2022-01-06 08:32:09 -0500

    added all migrations for tables, /theaters routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+const theatersRouter = require('./theaters/theaters.router')
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/theaters', theatersRouter)
 
 module.exports = app;
---
+++ src/errors/asyncErrorBoundary.js
@@ -0,0 +1,19 @@
+/* 
+Middleware to assist in catching asynchronous errors
+*/
+
+function asyncErrorBoundary(delegate, defaultStatus) {
+  return (request, response, next) => {
+    Promise.resolve()
+      .then(() => delegate(request, response, next))
+      .catch((error = {}) => {
+        const { status = defaultStatus, message = error } = error;
+        next({
+          status,
+          message,
+        });
+      });
+  };
+}
+
+module.exports = asyncErrorBoundary;
